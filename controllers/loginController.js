5b6e205764489f27bb6988e35f31bddcb048d9bc
manojsravan <manojsravan2002@gmail.com>
2024-05-15 22:44:54 +0530

    database model is ready

---
+++ controllers/loginController.js
@@ -0,0 +1,32 @@
+const Employee = require('../modles/employeeModel');
+const bcrypt = require('bcrypt');
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        
+        //@checking if user is present
+        const existingUser = await Employee.findOne({ email: email });
+        if (existingUser) {
+            return res.status(400).send('User is already present, please login instead');
+        }
+
+        //@ Generate salt and hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // @ user creation
+        const newUser = new Employee({ name, email, password: hashedPassword });
+        const createUser = await newUser.save();
+
+        if (createUser) {
+            return res.status(201).send(createUser);  
+        } else {
+            return res.status(400).send('User could not be created');
+        }
+    } catch (e) {
+        return res.status(500).send('Internal error: ' + e.message);
+    }
+}
+
+module.exports = registerUser;
---
+++ modles/employeeModel.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const employeeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required:true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+     isAdmin:{
+      type:Boolean,
+      required:true
+     },
+    department: {
+      type: String,
+     
+    },
+    position: {
+      type: String,
+    },
+    dateOfBirth: {
+      type: Date,
+       
+    },
+    address: {
+      street: String,
+      city: String,
+      state: String,
+      zip: String,
+      country: String,
+    },
+    phoneNumber: {
+      type: String,
+    
+    },
+    dateOfJoining: {
+      type: Date,
+      default: Date.now,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
+  },
+  { timestamps: true }
+);
+const Employee = mongoose.model("Employee", employeeSchema);
+module.exports = Employee;
