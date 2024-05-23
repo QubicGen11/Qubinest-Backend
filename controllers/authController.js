3c160c254700f6ec67108033c76fb0b1f20e8a1c
manojsravan <manojsravan2002@gmail.com>
2024-05-23 22:45:36 +0530

    updated backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
-const User = require('../models/userModel');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const Session = require('../models/sessionModel');
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const Session = require("../models/sessionModel");
 const jwtSecret = process.env.jwtSecret;
 
 // Register user
@@ -11,46 +11,57 @@ const userRegister = async (req, res) => {
 
     const existingUser = await User.findOne({ username });
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: "User already exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ username, password: hashedPassword, role });
     await newUser.save();
 
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    console.error('Error registering user:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error registering user:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
-// Login user
 const userLogin = async (req, res) => {
   try {
     const { username, password } = req.body;
 
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(401).json({ message: 'Invalid username or password' });
+      return res.status(401).json({ message: "Invalid username or password" });
     }
 
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
-      return res.status(401).json({ message: 'Invalid username or password' });
+      return res.status(401).json({ message: "Invalid username or password" });
     }
 
-    const session = new Session({ username, userId: user._id });
-    await session.save();
+    let session = await Session.findOne({ username });
 
-    const token = jwt.sign({ userId: user._id }, jwtSecret, { expiresIn: '1d' });
-    res.cookie('jwt', token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
-    res.cookie('username', username, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+    if (!session) {
+      // Insert new session
+      session = new Session({ username, userId: user._id }); // Set userId here
+      await session.save();
+    }
 
-    res.status(200).json({ message: 'Login successful', user: { username: user.username } });
+    const token = jwt.sign({ userId: user._id }, jwtSecret, {
+      expiresIn: "1d",
+    });
+    res.cookie("jwt", token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+    res.cookie("username", username, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res
+      .status(200)
+      .json({ message: "Login successful", user: { username: user.username } });
   } catch (error) {
-    console.error('Error logging in:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error logging in:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -59,14 +70,14 @@ const logout = async (req, res) => {
   try {
     const { username } = req.cookies;
 
-    res.clearCookie('jwt');
-    res.clearCookie('username');
+    res.clearCookie("jwt");
+    res.clearCookie("username");
     await Session.findOneAndDelete({ username });
 
-    res.status(200).json({ message: 'Logout successful' });
+    res.status(200).json({ message: "Logout successful" });
   } catch (error) {
-    console.error('Error logging out:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error logging out:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
